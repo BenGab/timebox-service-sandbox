ba3f64aa00235d676ff75a21e8da8d69e5722bd6
Gabor Benko <benkogabor@msn.com>
2021-02-11 17:16:10 +0100

    goroutine

--- src/main.go
+++ src/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	controller "github.com/bengab/timebox-service/src/controllers"
 )
 
-func main() {
+func startWebService(wg *sync.WaitGroup) {
 	ctrl := controller.NewTimeController()
-
+	defer wg.Done()
 	http.Handle("/api/timestamp", ctrl)
 	http.ListenAndServe(":8088", nil)
+
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+
+	go startWebService(wg)
+
+	wg.Wait()
 }
